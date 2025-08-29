2ddf6718f9740770d0d28d85be05565050c824bb
Esmond <esmondmccain@gmail.com>
2025-08-29 16:02:02 -0500

    refactor classes

--- em-block-collection.php
+++ em-block-collection.php
@@ -1,14 +1,5 @@
 <?php
 /**
- * Main plugin file for EM FAQ Accordion.
- * PHP version 7.3+
- *
- * @category  Wordpress_Plugin
- * @package   Esmond-M
- * @author    Esmond Mccain <esmondmccain@gmail.com>
- * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
- * @link      https://esmondmccain.com/
- *
  * Plugin Name:       EM Block Collection
  * Description:       A collection of custom Gutenberg blocks.
  * Requires at least: 6.1
@@ -17,25 +8,19 @@
  * Author:            Esmond Mccain
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       em-block-faq-accordion
- *
- * @see       https://developer.wordpress.org/block-editor/developers/block-api/
+ * Text Domain:       em-block-collection
  */
 
 declare(strict_types=1);
-namespace emBlockCollection;
 
-
-
-use emBlockFaqAccordion\emBlockFaqAccordion;
-use emBlockCarousel\emBlockCarousel;
-use emBlockPostGrid\emBlockPostGrid;
 defined('ABSPATH') or die();
 
+// Autoload classes if using Composer, otherwise require manually
 require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockFaqAccordion.php';
 require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockCarousel.php';
 require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockPostGrid.php';
 
-new emBlockFaqAccordion();
-new emBlockCarousel();
-new emBlockPostGrid();
+// Instantiate block classes
+new \emBlockFaqAccordion\emBlockFaqAccordion();
+new \emBlockCarousel\emBlockCarousel();
+new \emBlockPostGrid\emBlockPostGrid();
