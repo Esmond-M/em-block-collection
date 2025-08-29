484889a2e5c96b20ef974c801e67cc4fe08efa25
Esmond <esmondmccain@gmail.com>
2025-08-29 03:46:38 -0500

    point to new folder structure

--- includes/classes/emBlockCarousel.php
+++ includes/classes/emBlockCarousel.php
@@ -117,11 +117,11 @@ class emBlockCarousel
      */
     public function em_block_carousel_init()
     {
-        register_block_type(
-            dirname(__DIR__, 2) . '/build',
-            [
-                'render_callback' => [$this, 'em_block_carousel_content']
-            ]
-        );
+    register_block_type(
+      dirname(__DIR__, 2) . '/build/carousel',
+      [
+        'render_callback' => [$this, 'em_block_carousel_content']
+      ]
+    );
     }
 }
