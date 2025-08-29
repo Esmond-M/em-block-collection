4f95d4ac283a0a36eb30ecd04955aeecd688e2dd
Esmond <esmondmccain@gmail.com>
2025-08-29 02:35:33 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
+const { merge } = require('webpack-merge');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
-module.exports = {
-    ...defaultConfig,
+module.exports = merge(defaultConfig, {
     entry: {
         'faq-accordion/index': path.resolve(__dirname, 'src/faq-accordion/index.js'),
+        'faq-accordion/view': path.resolve(__dirname, 'src/faq-accordion/view.js'),
         'new-block/index': path.resolve(__dirname, 'src/new-block/index.js'),
     },
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js',
     },
-};
+});
