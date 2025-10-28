855c00f17ce175d18bce0c681f3b55107a82f984
Esmond <esmondmccain@gmail.com>
2025-10-28 14:10:13 -0500

    create new category and include space divider

--- em-block-collection.php
+++ em-block-collection.php
@@ -15,12 +15,31 @@ declare(strict_types=1);
 
 defined('ABSPATH') or die();
 
+/**
+ * Register custom block category for EM Blocks
+ */
+function em_block_collection_register_category($categories) {
+    return array_merge(
+        [
+            [
+                'slug'  => 'em-blocks',
+                'title' => __('EM Blocks', 'em-block-collection'),
+                'icon'  => 'admin-plugins',
+            ],
+        ],
+        $categories
+    );
+}
+add_filter('block_categories_all', 'em_block_collection_register_category');
+
 // Include block classes
 require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockFaqAccordion.php';
 require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockCarousel.php';
 require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockPostGrid.php';
+require_once plugin_dir_path(__FILE__) . 'includes/classes/emBlockSpacerDivider.php';
 
 // Instantiate block classes
 new \emBlockCollection\emBlockFaqAccordion();
 new \emBlockCollection\emBlockCarousel();
 new \emBlockCollection\emBlockPostGrid();
+new \emBlockCollection\emBlockSpacerDivider();
