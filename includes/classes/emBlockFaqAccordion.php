6f50b0d19a66cf8735250adba08e80a0a314e77b
Esmond <esmondmccain@gmail.com>
2025-10-28 15:02:15 -0500

    Add missing docblock for emBlockFaqAccordion class

--- includes/classes/emBlockFaqAccordion.php
+++ includes/classes/emBlockFaqAccordion.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Main plugin class for EM FAQ Accordion.
+ * PHP version 7.3+
+ * @category Wordpress_Plugin
+ * @package  Esmond-M
+ * @author   Esmond Mccain <esmondmccain@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
+ * @link     esmondmccain.com
+ */
 declare(strict_types=1);
 namespace emBlockCollection;
 
